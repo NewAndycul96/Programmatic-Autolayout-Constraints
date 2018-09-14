ed05dd14ecab83a8e7192ec5f507fec8bdde8a64
NewAndycul96 <andycul96@yahoo.com>
2018-09-14 18:55:18 -0500

    First file done

--- Programmatic Autolayout Constraints/ViewController.swift
+++ Programmatic Autolayout Constraints/ViewController.swift
@@ -10,8 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var imageView = UIImageView()
+    
+    var leadingConstraint: NSLayoutConstraint?
+    var trailingConstraint: NSLayoutConstraint?
+    var topConstraint: NSLayoutConstraint?
+    var bottomConstraint: NSLayoutConstraint?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageView.image = UIImage(named: "goku.jpeg")
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        view.addSubview(imageView)
+        
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        leadingConstraint = NSLayoutConstraint(item: imageView, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1.0, constant: 20.0)
+        
+        trailingConstraint = NSLayoutConstraint(item: imageView, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1.0, constant: -20.0)
+        
+        topConstraint = NSLayoutConstraint(item: imageView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: 20.0)
+        
+        bottomConstraint = NSLayoutConstraint(item: imageView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1.0, constant: -20.0)
+        
+        leadingConstraint?.isActive = true
+        trailingConstraint?.isActive = true
+        topConstraint?.isActive = true
+        bottomConstraint?.isActive = true
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
